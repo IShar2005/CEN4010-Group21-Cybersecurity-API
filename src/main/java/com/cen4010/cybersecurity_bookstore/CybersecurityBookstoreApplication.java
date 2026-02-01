84cb903b935c96c12c544efefcdf805a1f830146
Isa Sharief <ishar008@fiu.edu>
2026-02-01 13:42:31 -0500

    Sprint 1: Initial environment and Spring Boot setup complete

---
+++ src/main/java/com/cen4010/cybersecurity_bookstore/CybersecurityBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.cen4010.cybersecurity_bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybersecurityBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CybersecurityBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cen4010/cybersecurity_bookstore/CybersecurityBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cen4010.cybersecurity_bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybersecurityBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
